b4f0a119adb08144700f473220b127a2c5857cdc
aaa15test <aaa15test15aaa@mail.ru>
2023-03-14 10:07:32 +0300

    put all the main content of the page in the main tag; set the correct page language

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require('fs');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/*', (req, res) => {
   res.sendFile('index.html', { root: __dirname + '/dist/'});
